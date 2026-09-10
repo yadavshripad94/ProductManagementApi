64f68801930bc01e0bf03a3600f055e459aa9829
yadavshripad94 <yadavshripad94@gmail.com>
2026-09-10 15:37:43 +0530

    HealthController added

---
+++ ProductManagementApi/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Status = "Healthy",
+                Message = "Product Management API is running successfully."
+            });
+        }
+
+    }
+}
